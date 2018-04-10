d77aa83172d2dcd0fd3da288369f3e98d67a4ef8
Chelsi Jones <cajones.bz@gmail.com>
2018-04-10 09:48:28 -0500

    Refine Flight's duration attribute

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,11 @@
 class Flight < ApplicationRecord
+    after_initialize :calculate_duration
+
     belongs_to :from_airport, class_name: "Airport"
     belongs_to :to_airport, class_name: "Airport"
+
+    private
+    def calculate_duration
+        self.update_attribute(:duration, (Time.at(self.arrival - self.departure).utc.strftime("%H:%M:%S")))
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+    Airport.create(name: Array.new(3){ [*"A".."Z"].sample }.join)
+end
